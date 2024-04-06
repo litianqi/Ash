e353d461165dd645464e540d7f300504c7f264c1
Tianqi Li <litianqi@outlook.com>
2024-04-06 18:00:28 +0800

    Add CLion C++ file templates

---
+++ .idea/fileTemplates/internal/C Source File.c
@@ -0,0 +1,7 @@
+#if (${HEADER_FILENAME})
+#[[#include]]# "${HEADER_FILENAME}"
+#end
+
+namespace ash
+{
+} // namespace ash
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace ash
+{
+class ${NAME} {
+
+};
+} // namespace ash
