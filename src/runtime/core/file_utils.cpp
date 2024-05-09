daf4e636ac46253881213e7345f55f161e1b4abb
Tianqi Li <litianqi@outlook.com>
2024-05-10 03:03:20 +0800

    Add #include support for read_shader() function

--- src/runtime/core/file_utils.cpp
+++ src/runtime/core/file_utils.cpp
@@ -1,4 +1,5 @@
 #include "file_utils.h"
+#include <regex>
 #include <fstream>
 #include <algorithm>
 #include <vector>
@@ -31,6 +32,28 @@ std::variant<std::string, FileError> read_text_file(const fs::path& file_path)
 std::variant<std::string, FileError> read_shader(const fs::path& relative_file_path)
 {
     auto file_path = BaseApp::get()->get_shaders_dir() / relative_file_path;
-    return read_text_file(file_path);
+    auto result = read_text_file(file_path);
+    if (auto* file = std::get_if<std::string>(&result))
+    {
+        std::regex rgx("#include\\s+\"(.+)\"");
+        std::smatch matches;
+
+        while (std::regex_search(*file, matches, rgx))
+        {
+            assert(matches.size() == 2);
+            auto& match = matches[1];
+            auto include_result = read_shader(match.str());
+            if (auto* include_file = std::get_if<std::string>(&include_result))
+            {
+                file->replace(matches.position(), matches.length(), *include_file);
+            }
+            else
+            {
+                spdlog::error("Failed to find include file `{}`", match.str());
+                return include_result;
+            }
+        }
+    }
+    return result;
 }
 } // namespace ash
